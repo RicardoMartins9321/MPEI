395bac28ca7285268d19483faf4424cd1b55aa2f
Ricardo Martins <114387040+RicardoMartins9321@users.noreply.github.com>
2023-11-16 06:26:08 +0000

    Delete MPEI_Práticas/Guião 1 directory

--- MPEI_Práticas/Guião 1/Guião 2/2.2/prob_teorica.m
+++
@@ -1,8 +0,0 @@
-% função que dá as probabilidades teoricas de em n experiencias acontecerem
-% k situações tendo em conta a probabilidade p de estes acontecerem
-
-function p = prob_teorica(k,n,p)
-
-p = nchoosek(n,k)*p^k*(1-p)^(n-k); 
-
-end
--- MPEI_Práticas/Guião 1/probCalculo.m
+++
@@ -1,6 +0,0 @@
-function [prob] = probCalculo(p,k,n)
-
-% faz o cálculo da probabilidade analítica através da seguintes fórmula
-prob = nchoosek(n,k)*p^k*(1-p)^(n-k); % nchoosek(n,k)= n!/(n-k)!/k!h
-
-end
